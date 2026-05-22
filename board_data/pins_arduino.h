b32494bdd588c4b6d0f2f5f6cc1a7ead6bbd62eb
Sara Damiano <sdamiano@stroudcenter.org>
2026-05-22 16:54:27 -0400

    format changes, add defines for port expander
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- board_data/pins_arduino.h
+++ board_data/pins_arduino.h
@@ -32,16 +32,15 @@
  *
  * To be used in comparisons, such as ${board_name_upper}_VERSION >= ${board_name_upper}_VERSION_VAL(4, 0, 0)
  */
-#define ${board_name_upper}_VERSION ${board_name_upper}_VERSION_VAL(${board_name_upper}_VERSION_MAJOR, \
-                                                                       ${board_name_upper}_VERSION_MINOR, \
-                                                                       ${board_name_upper}_VERSION_PATCH)
+#define ${board_name_upper}_VERSION ${board_name_upper}_VERSION_VAL(${board_name_upper}_VERSION_MAJOR, ${board_name_upper}_VERSION_MINOR, ${board_name_upper}_VERSION_PATCH)
 // clang-format on
 
 /*----------------------------------------------------------------------------
  *        Pins
  *----------------------------------------------------------------------------*/
 
-// ATMEL ATMEGA1284P
+// clang-format off
+// ATMEL ATMEGA1284P Process Diagram
 // (Notice that EnviroDIY Mayfly is equipped with TQFN package. The layout below is
 // just a convenience to see the port names and their usage.)
 //
@@ -75,31 +74,25 @@
 //                                                                   XBEE
 //                                                                   CTS
 //                                                   |--ICR BIT 2 (C)--|
+// clang-format on
 
 /*
  * 0..32
  * PD0..PD7, PB0..PB7, PC0..PC7, PA0..PA7
  */
 
+// pin counts
 #define NUM_DIGITAL_PINS (32u)
 #define NUM_ANALOG_INPUTS (8u)
-
-// Mapping from analog pin number to digital pin number
-// This can be a function if pin numbers are contiguous or it can specify the mapping for each pin explicitly.
-#define analogInputToDigitalPin(p) ((p < NUM_ANALOG_INPUTS) ? (p) + 24 : -1)
-#define digitalPinToInterrupt(p) ((p) == 10 ? 2 : ((p) == 2 ? 0 : ((p) == 3 ? 1 : NOT_AN_INTERRUPT)))
-
-#define digitalPinHasPWM(p) ((p) == 11 || (p) == 12 || (p) == 14 || (p) == 15 || (p) == 4 || (p) == 5 || (p) == 6 || (p) == 7)
-
-// LEDs
-// Optional macros for user LEDs.
-static const uint8_t LED2 = 8; // Green
-static const uint8_t LED1 = 9; // Red
 
 /**
  * Analog pin definitions
  * Explicitly define each analog pin number to its corresponding digital pin number
  */
+
+// Mapping from analog pin number to digital pin number
+// This can be a function if pin numbers are contiguous or it can specify the mapping for each pin explicitly.
+#define analogInputToDigitalPin(p) ((p < NUM_ANALOG_INPUTS) ? (p) + 24 : -1)
 
 // Standard analog pins as defines
 static const uint8_t A0 = 24;
@@ -112,17 +105,32 @@ static const uint8_t A6 = 30;
 static const uint8_t A7 = 31;
 
 /**
- * Pin-Change Interrupt Mapping
-   PCINT31-24: D7-0   : bit 3(D)
-   PCINT15-8:  D15-8  : bit 1(B)
-   PCINT23-16: D23-16 : bit 2(C)
-   PCINT7-0:   D31-24 : bit 0(A) (also A0..A7)
-*/
+ * Interrupt Mapping
+ * External Interrupts have a have a dedicated mapping - one pin per interrupt.
+ *     D2 (INT0), D3 (INT1), D10 (INT2)
+ * Pin Change Interrupts are mapped to ports.
+ *     PCINT31-24: D7-0   : bit 3(D)
+ *     PCINT15-8:  D15-8  : bit 1(B)
+ *     PCINT23-16: D23-16 : bit 2(C)
+ *     PCINT7-0:   D31-24 : bit 0(A) (also A0..A7)
+ */
 
+// Mapping from digital pin number to EXTERNAL interrupt number
+#define digitalPinToInterrupt(p) ((p) == 10 ? 2 : ((p) == 2 ? 0 : ((p) == 3 ? 1 : NOT_AN_INTERRUPT)))
+
+// Mapping from digital pin number to pin change interrupt control register (PCICR) and bit
 #define digitalPinToPCICR(p) (((p) >= 0 && (p) < NUM_DIGITAL_PINS) ? (&PCICR) : ((uint8_t *)0))
 #define digitalPinToPCICRbit(p) (((p) <= 7) ? 3 : (((p) <= 15) ? 1 : (((p) <= 23) ? 2 : 0)))
+// Mapping from digital pin number to pin change mask register (PCMSK) and bit
 #define digitalPinToPCMSK(p) (((p) <= 7) ? (&PCMSK3) : (((p) <= 15) ? (&PCMSK1) : (((p) <= 23) ? (&PCMSK2) : (&PCMSK0))))
 #define digitalPinToPCMSKbit(p) ((p) % 8)
+
+/**
+ * Pulse Width Modulation (PWM) Mapping
+ */
+
+// Boolean function to check if a digital pin supports PWM
+#define digitalPinHasPWM(p) ((p) == 11 || (p) == 12 || (p) == 14 || (p) == 15 || (p) == 4 || (p) == 5 || (p) == 6 || (p) == 7)
 
 #ifdef ARDUINO_MAIN
 
@@ -283,11 +291,21 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] =
  *
  * SPI – Serial Peripheral Interface (Host operation)
  */
+// SD Card SPI
+#define PIN_SPI_MOSI (13)
+// ^ Digital pin for SPI Data Out (MOSI as host, MISO as client)
+#define PIN_SPI_SCK (15)
+// ^ Digital pin for SPI SCK
+#define PIN_SPI_SS (12)
+// ^ Digital pin for SPI CS
+#define PIN_SPI_MISO (14)
+// ^ Digital pin for SPI Data In (MISO as host, MOSI as client)
+
 // static constants for the SPI pins
-static const uint8_t SS = 12;
-static const uint8_t MOSI = 13;
-static const uint8_t MISO = 14;
-static const uint8_t SCK = 15;
+static const uint8_t SS = PIN_SPI_SS;
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK = PIN_SPI_SCK;
 
 /**
  * Wire (I2C) Interfaces
@@ -303,6 +321,13 @@ static const uint8_t SCL = 16;
  * Put other defines that will be convenient for your users or libraries here.
  *----------------------------------------------------------------------------*/
 
+// LEDs
+// Optional macros and static constants for user LEDs.
+#define LED_BUILTIN 8
+static const uint8_t LED2 = 8; // Green
+static const uint8_t LED1 = 9; // Red
+
+// X-Bee Socket
 #define SerialBee Serial1
 static const uint8_t BEEPWR = 18;
 static const uint8_t BEERX = 11;
@@ -313,13 +338,42 @@ static const uint8_t BEECTS = 19;
 static const uint8_t BEESTATUS = 19; // must switch solder jumper
 static const uint8_t BEERESET = 29;  // A5=29
 
+// Grove ports
 static const uint8_t GROVEPWR = 22;
 static const uint8_t GROVEPWR_OFF = 0;
 static const uint8_t GROVEPWR_ON = 1;
 
+// Battery voltage measurement
 static const uint8_t BATVOLTPIN = 30; // A6
 #define BATVOLT_R1 47                 // in fact 4.7M
 #define BATVOLT_R2 100                // in fact 10M
+
+#if defined(MAYFLY_VERSION) && defined(MAYFLY_VERSION_VAL) && MAYFLY_VERSION >= MAYFLY_VERSION_VAL(2, 0, 0)
+
+#define SC16IS7XX_DEFAULT_ADDRESS \
+    (0X9A) ///< A0 tied to GND, A1 tied to GND
+#define SC16IS7XX_DEFAULT_XTAL_FREQ \
+    (3686000UL) ///< The default frequency of the crystal in hertz
+
+// External serial chip
+// Port expander pins as static constants
+static const uint8_t X0 = 0;
+static const uint8_t X1 = 1;
+static const uint8_t X2 = 2;
+static const uint8_t X3 = 3;
+static const uint8_t X4 = 4;
+static const uint8_t X5 = 5;
+static const uint8_t X6 = 6;
+static const uint8_t X7 = 7;
+
+// #include <SC16IS752.h>
+
+// SC16IS752 Serial2(SC16IS752_CHANNEL_A);
+// SC16IS752 Serial3(SC16IS752_CHANNEL_B);
+
+// extern SC16IS752 Serial2;
+// extern SC16IS752 Serial3;
+#endif
 
 #endif // Pins_Arduino_h
 // vim:ai:cin:sts=2 sw=2 ft=cpp
